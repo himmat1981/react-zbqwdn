f1a13b0abd1143985f0b1453a8499589c985620f
himmat1981 <himmatsinghbhatia@gmail.com>
2019-11-25 16:42:03 +0530

    2 files modified

--- about.js
+++ about.js
@@ -3,7 +3,7 @@ import React from "react";
 const About = () => {
   return (
      <div>
-       <p > This is about us page </p>
+       <p > This is about us page This is about us page This is about us pageThis is about us pageThis is about us pageThis is about us pageThis is about us pageThis is about us pageThis is about us pageThis is about us pageThis is about us pageThis is about us pageThis is about us pageThis is about us pageThis is about us pageThis is about us pageThis is about us pageThis is about us pageThis is about us pageThis is about us pageThis is about us page  </p>
      </div>
   );
 }
